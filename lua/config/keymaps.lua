dad5c0428e337000e4b935cb042b98680db0bd90
fuchami <fuchami23@icloud.com>
2025-07-02 22:24:57 +0900

    add window keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local map = vim.keymap.set
+
+-- Window
+map("n", "-", "<Cmd>split<CR>", { desc = "Horizontal Split Window" })
+map("n", "|", "<Cmd>vsplit<CR>", { desc = "Vertical Split Window" })
+
+map("n", "<A-l>", ":vertical resize +2<CR>")
+map("n", "<A-h", ":vertical resize -2<CR>")
+map("n", "<A-j>", ":resize -2<CR>")
+map("n", "<A-k>", ":resize +2<CR>")

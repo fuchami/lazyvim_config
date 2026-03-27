49d7210a21dda2c32082bcbc29387a017ed80b73
fuchami <fuchami23@icloud.com>
2026-03-28 00:34:12 +0900

    add kanso colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,4 @@
+---@type LazySpec
 return {
   {
     "rebelot/kanagawa.nvim",
@@ -6,6 +7,24 @@ return {
       compile = true,
       transparent = true,
       theme = "wave",
+      background = { -- map the value of 'background' option to a theme
+        dark = "wave", -- try "dragon" !
+        light = "lotus",
+      },
+    },
+  },
+  {
+    "webhooked/kanso.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      background = { -- map the value of 'background' option to a theme
+        dark = "mist", -- try "zen", "ink", "mist" or "pearl" !
+        light = "pearl", -- try "zen", "mist" or "ink" !
+      },
+      foreground = "default", -- "default" or "saturated" (can also be a table like background)
+      minimal = false, -- reduced color palette for a more minimal look
     },
   },
 
@@ -17,6 +36,7 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "kanagawa",
+      -- colorscheme = "kanso",
     },
   },
 }

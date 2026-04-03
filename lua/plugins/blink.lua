a266978c51cb03806d029604f508468902d19aef
fuchami <fuchami23@icloud.com>
2026-04-04 02:34:48 +0900

    feat: set blink keymap

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,31 @@
+local function has_words_before()
+  local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = {
+          "select_next",
+          "snippet_forward",
+          function(cmp)
+            if has_words_before() or vim.api.nvim_get_mode().mode == "c" then return cmp.show() end
+          end,
+          "fallback",
+        },
+        ["<S-Tab>"] = {
+          "select_prev",
+          "snippet_backward",
+          function(cmp)
+            if vim.api.nvim_get_mode().mode == "c" then return cmp.show() end
+          end,
+          "fallback",
+        },
+      },
+    },
+  },
+}

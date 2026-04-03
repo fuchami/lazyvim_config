86bb0fc97ce3fabece1fca81bb170f6981fb33d6
fuchami <fuchami23@icloud.com>
2026-04-04 02:35:47 +0900

    feat(keymap): set comment, window resize keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+--  Toggle Comment
+vim.keymap.set("n", "<leader>/", "gcc", { remap = true, desc = "Toggle comment line" })
+vim.keymap.set("x", "<leader>/", "gc", { remap = true, desc = "Toggle comment" })
+
+-- Window resize
+vim.keymap.set("n", "<C-A-l>", ":vertical resize +2<CR>")
+vim.keymap.set("n", "<C-A-h>", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<C-A-j>", ":resize -2<CR>")
+vim.keymap.set("n", "<C-A-k>", ":resize +2<CR>")
